c94952e3af4b04dfb3d8401e73aab887f4e4ff17
pxai <pello_altadill@cuatrovientos.org>
2016-01-17 22:38:21 +0100

    OK, now everything was added

---
+++ Assets/LoadStart.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadStart : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("3 seconds to start");
+		Invoke ("loadStart", 3);
+	}
+
+	public void loadStart () {
+		Debug.Log ("Loading start level");
+		Application.LoadLevel("StartScene");
+	}
+}
---
+++ Assets/SceneChangeScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneChangeScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void loadScene (string name) {
+		Debug.Log ("Loading level " + name);
+		Application.LoadLevel(name);
+	}
+}
